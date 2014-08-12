52ba481868d19d835121ea79243ad7f844d8c261
Gretchen Moran <gmoran@pentaho.com>
2014-08-12 11:51:20 -0400

    [PDI-10610]: 	Resolve transformation and job entity nodes missing from graph

---
+++ src/main/java/com/pentaho/metaverse/analyzer/kettle/BaseDocumentAnalyzer.java
@@ -0,0 +1,75 @@
+package com.pentaho.metaverse.analyzer.kettle;
+
+import com.pentaho.dictionary.DictionaryConst;
+import com.pentaho.metaverse.analyzer.kettle.jobentry.GenericJobEntryMetaAnalyzer;
+import com.pentaho.metaverse.analyzer.kettle.jobentry.IJobEntryAnalyzer;
+import com.pentaho.metaverse.messages.Messages;
+import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.job.Job;
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.job.entry.JobEntryCopy;
+import org.pentaho.di.job.entry.JobEntryInterface;
+import org.pentaho.platform.api.metaverse.IDocumentAnalyzer;
+import org.pentaho.platform.api.metaverse.IMetaverseDocument;
+import org.pentaho.platform.api.metaverse.IMetaverseNode;
+import org.pentaho.platform.api.metaverse.MetaverseAnalyzerException;
+
+import java.io.ByteArrayInputStream;
+import java.util.Date;
+import java.util.Set;
+
+/**
+ * Created by gmoran on 8/11/14.
+ */
+public abstract class BaseDocumentAnalyzer extends BaseKettleMetaverseComponent implements IDocumentAnalyzer {
+
+  /**
+   * This method handles null checks for state validation
+   *
+   * @param document the document that will be processed in non-abstract subclasses
+   * @throws MetaverseAnalyzerException
+   */
+  protected void validateState(IMetaverseDocument document) throws MetaverseAnalyzerException{
+
+    if ( document == null ) {
+      throw new MetaverseAnalyzerException( Messages.getString( "ERROR.Document.IsNull" ) );
+    }
+
+    Object repoObject = document.getContent();
+
+    if ( repoObject == null ) {
+      throw new MetaverseAnalyzerException( Messages.getString( "ERROR.Document.HasNoContent" ) );
+    }
+
+    if ( metaverseObjectFactory == null ) {
+      throw new MetaverseAnalyzerException( Messages.getString( "ERROR.MetaverseObjectFactory.IsNull" ) );
+    }
+
+    if ( metaverseBuilder == null ) {
+      throw new MetaverseAnalyzerException( Messages.getString( "ERROR.MetaverseBuilder.IsNull" ) );
+    }
+
+  }
+
+  /**
+   * This method creates the document
+   * node relationships with the system level structural and data nodes in the graph.
+   *
+   * @param child the document node
+   * @throws MetaverseAnalyzerException
+   */
+  public void addParentLink( IMetaverseNode child) throws MetaverseAnalyzerException {
+
+    // The document is always a child of the locator. If this nis not the case, then do not
+    // subclass this document analyzer.
+    //
+    // This will create a virtual node that will line up with the correct
+    // locator node for this document in the graph.
+    IMetaverseNode locatorNode = metaverseObjectFactory.createNodeObject(
+        getNamespace().getParentNamespace().getNamespaceId() );
+
+    metaverseBuilder.addLink( locatorNode, DictionaryConst.LINK_CONTAINS, child );
+
+  }
+
+}
