1729ee9dbe76b85c20b62c54d84fce72b3185480
Matt Burgess <mburgess@pentaho.com>
2014-09-17 13:00:56 -0400

    [PDI-10610]: Add NumberRangeStepAnalyzer

---
+++ src/main/java/com/pentaho/metaverse/analyzer/kettle/step/NumberRangeStepAnalyzer.java
@@ -0,0 +1,80 @@
+/*!
+ * PENTAHO CORPORATION PROPRIETARY AND CONFIDENTIAL
+ *
+ * Copyright 2002 - 2014 Pentaho Corporation (Pentaho). All rights reserved.
+ *
+ * NOTICE: All information including source code contained herein is, and
+ * remains the sole property of Pentaho and its licensors. The intellectual
+ * and technical concepts contained herein are proprietary and confidential
+ * to, and are trade secrets of Pentaho and may be covered by U.S. and foreign
+ * patents, or patents in process, and are protected by trade secret and
+ * copyright laws. The receipt or possession of this source code and/or related
+ * information does not convey or imply any rights to reproduce, disclose or
+ * distribute its contents, or to manufacture, use, or sell anything that it
+ * may describe, in whole or in part. Any reproduction, modification, distribution,
+ * or public display of this information without the express written authorization
+ * from Pentaho is strictly prohibited and in violation of applicable laws and
+ * international treaties. Access to the source code contained herein is strictly
+ * prohibited to anyone except those individuals and entities who have executed
+ * confidentiality and non-disclosure agreements or other agreements with Pentaho,
+ * explicitly covering such access.
+ */
+
+package com.pentaho.metaverse.analyzer.kettle.step;
+
+import com.pentaho.dictionary.DictionaryConst;
+import org.pentaho.di.trans.step.BaseStepMeta;
+import org.pentaho.di.trans.steps.numberrange.NumberRangeMeta;
+import org.pentaho.platform.api.metaverse.IMetaverseComponentDescriptor;
+import org.pentaho.platform.api.metaverse.IMetaverseNode;
+import org.pentaho.platform.api.metaverse.MetaverseAnalyzerException;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * The SelectValuesStepAnalyzer is responsible for providing nodes and links (i.e. relationships) between for the
+ * fields operated on by Select Values steps.
+ */
+public class NumberRangeStepAnalyzer extends BaseStepAnalyzer<NumberRangeMeta> {
+
+  /**
+   * Analyzes Number Range steps to determine the various operations performed on fields and their data
+   * 
+   * @see org.pentaho.platform.api.metaverse.IAnalyzer#analyze(IMetaverseComponentDescriptor,java.lang.Object)
+   */
+  @Override
+  public IMetaverseNode analyze(
+      IMetaverseComponentDescriptor descriptor, NumberRangeMeta numberRangeMeta ) throws MetaverseAnalyzerException {
+
+    // Do common analysis for all steps
+    super.analyze( descriptor, numberRangeMeta );
+
+    String inputFieldName = numberRangeMeta.getInputField();
+    String outputFieldName = numberRangeMeta.getOutputField();
+
+    if ( inputFieldName != null && outputFieldName != null ) {
+      // We can't use our own descriptor here, we need to get the descriptor for the origin step
+      IMetaverseNode inputFieldNode =
+          createNodeFromDescriptor( getPrevStepFieldOriginDescriptor( descriptor, inputFieldName ) );
+
+      // Not sure if we need a new node or not, but the builder will take care of it, so just create a node
+      // so we can add the "derives" link
+      IMetaverseComponentDescriptor outputFieldDescriptor = getStepFieldOriginDescriptor( descriptor, outputFieldName );
+      IMetaverseNode outputFieldNode = createNodeFromDescriptor( outputFieldDescriptor );
+
+      metaverseBuilder.addLink( rootNode, DictionaryConst.LINK_USES, inputFieldNode );
+      metaverseBuilder.addLink( inputFieldNode, DictionaryConst.LINK_DERIVES, outputFieldNode );
+    }
+    return rootNode;
+  }
+
+  @Override
+  public Set<Class<? extends BaseStepMeta>> getSupportedSteps() {
+    return new HashSet<Class<? extends BaseStepMeta>>() {
+      {
+        add( NumberRangeMeta.class );
+      }
+    };
+  }
+}
