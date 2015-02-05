9bb4933ebb42bdfa3a66231268bd1f4c8ba68e04
wseyler <wseyler@pentaho.com>
2015-02-05 11:01:31 -0500

    [BACKLOG-525] - Implemented custom metaverse step analyzer for calculator.  Added unit and integration tests.  Also added integration test for StreamLookupStepAnalyzer.

--- src/main/java/com/pentaho/metaverse/analyzer/kettle/step/streamlookup/StreamLookupStepAnalyzer.java
+++ src/main/java/com/pentaho/metaverse/analyzer/kettle/step/streamlookup/StreamLookupStepAnalyzer.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.pentaho.commons.connection.Messages;
 import org.pentaho.di.core.exception.KettleStepException;
 import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.trans.TransMeta;
@@ -43,6 +42,7 @@ import org.slf4j.LoggerFactory;
 
 import com.pentaho.dictionary.DictionaryConst;
 import com.pentaho.metaverse.analyzer.kettle.step.BaseStepAnalyzer;
+import com.pentaho.metaverse.messages.Messages;
 
 import edu.emory.mathcs.backport.java.util.Arrays;
 
@@ -87,6 +87,7 @@ public class StreamLookupStepAnalyzer extends BaseStepAnalyzer<StreamLookupMeta>
 
       IMetaverseNode valueName = createNodeFromDescriptor( getStepFieldOriginDescriptor( descriptor, valueNames[i] ) );
       metaverseBuilder.addLink( valueNode, DictionaryConst.LINK_DERIVES, valueName );
+      metaverseBuilder.addLink( rootNode, DictionaryConst.LINK_CREATES, valueName );
     }
 
     return node;
