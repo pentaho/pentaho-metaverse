dd980fcf3213f3d6a2c7c63665f4ca276dd03b90
wseyler <wseyler@pentaho.com>
2015-02-16 13:48:22 -0500

    [BACKLOG-2160] - Implemented step analyzer for Excel Input step.  Added unit and IT tests.  Formatted per checkstyle and tested.
    [BACKLOG-2160] - Implemented step analyzer for Excel Input step.  Added unit and IT tests.  Formatted per checkstyle and tested. (+1 squashed commit)
    Squashed commits:
    [b0fea05] [BACKLOG-2160] - Implemented step analyzer for Excel Input step.  Added unit and IT tests.  Formatted per checkstyle and tested.

---
+++ src/main/java/com/pentaho/metaverse/analyzer/kettle/step/excelinput/ExcelInputStepAnalyzer.java
@@ -0,0 +1,118 @@
+/*
+ * PENTAHO CORPORATION PROPRIETARY AND CONFIDENTIAL
+ *
+ * Copyright 2002 - 2015 Pentaho Corporation (Pentaho). All rights reserved.
+ *
+ * NOTICE: All information including source code contained herein is, and
+ * remains the sole property of Pentaho and its licensors. The intellectual
+ * and technical concepts contained herein are proprietary and confidential
+ * to, and are trade secrets of Pentaho and may be covered by U.S. and foreign
+ * patents, or patents in process, and are protected by trade secret and
+ * copyright laws. The receipt or possession of this source code and/or related
+ * information does not convey or imply any rights to reproduce, disclose or
+ * distribute its contents, or to manufacture, use, or sell anything that it
+ * may describe, in whole or in part. Any reproduction, modification, distribution,
+ * or public display of this information without the express written authorization
+ * from Pentaho is strictly prohibited and in violation of applicable laws and
+ * international treaties. Access to the source code contained herein is strictly
+ * prohibited to anyone except those individuals and entities who have executed
+ * confidentiality and non-disclosure agreements or other agreements with Pentaho,
+ * explicitly covering such access.
+ */
+
+package com.pentaho.metaverse.analyzer.kettle.step.excelinput;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.pentaho.di.core.Const;
+import org.pentaho.di.trans.step.BaseStepMeta;
+import org.pentaho.di.trans.steps.excelinput.ExcelInputField;
+import org.pentaho.di.trans.steps.excelinput.ExcelInputMeta;
+import org.pentaho.platform.api.metaverse.IComponentDescriptor;
+import org.pentaho.platform.api.metaverse.IMetaverseNode;
+import org.pentaho.platform.api.metaverse.MetaverseAnalyzerException;
+import org.pentaho.platform.api.metaverse.MetaverseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pentaho.dictionary.DictionaryConst;
+import com.pentaho.metaverse.analyzer.kettle.step.BaseStepAnalyzer;
+import com.pentaho.metaverse.impl.MetaverseComponentDescriptor;
+
+/**
+ * The ExcelInputStepAnalyzer is responsible for providing nodes and links (i.e. relationships) between itself and other
+ * metaverse entities
+ */
+public class ExcelInputStepAnalyzer extends BaseStepAnalyzer<ExcelInputMeta> {
+  private Logger log = LoggerFactory.getLogger( ExcelInputStepAnalyzer.class );
+
+  @Override
+  public IMetaverseNode analyze( IComponentDescriptor descriptor, ExcelInputMeta excelInputMeta )
+    throws MetaverseAnalyzerException {
+
+    // do the common analysis for all step
+    IMetaverseNode node = super.analyze( descriptor, excelInputMeta );
+
+    // add the fields as nodes, add the links too
+    ExcelInputField[] fields = excelInputMeta.getField();
+    if ( fields != null ) {
+      for ( ExcelInputField field : fields ) {
+        String fieldName = field.getName();
+        IComponentDescriptor fileFieldDescriptor =
+            new MetaverseComponentDescriptor( fieldName, DictionaryConst.NODE_TYPE_FILE_FIELD, descriptor
+                .getNamespace(), descriptor.getContext() );
+        IMetaverseNode fieldNode = createNodeFromDescriptor( fileFieldDescriptor );
+        metaverseBuilder.addNode( fieldNode );
+
+        // Get the stream field output from this step. It should've already been created when we called super.analyze()
+        IComponentDescriptor transFieldDescriptor = getStepFieldOriginDescriptor( descriptor, fieldName );
+        IMetaverseNode outNode = createNodeFromDescriptor( transFieldDescriptor );
+
+        metaverseBuilder.addLink( fieldNode, DictionaryConst.LINK_POPULATES, outNode );
+
+        // add a link from the fileField to the text file input step node
+        metaverseBuilder.addLink( node, DictionaryConst.LINK_USES, fieldNode );
+      }
+    }
+
+    if ( excelInputMeta.isAcceptingFilenames() ) {
+      String acceptingFieldName = excelInputMeta.getAcceptingField();
+      IComponentDescriptor transFieldDescriptor = getPrevStepFieldOriginDescriptor( descriptor, acceptingFieldName );
+      IMetaverseNode acceptingFieldNode = createNodeFromDescriptor( transFieldDescriptor );
+
+      // add a link from the fileField to the excel file input step node
+      metaverseBuilder.addLink( node, DictionaryConst.LINK_USES, acceptingFieldNode );
+    } else {
+      String[] fileNames = parentTransMeta.environmentSubstitute( excelInputMeta.getFileName() );
+
+      // add a link from the file(s) being read to the step
+      if ( fileNames != null ) {
+        for ( String fileName : fileNames ) {
+          if ( !Const.isEmpty( fileName ) ) {
+            try {
+              // first add the node for the file
+              IMetaverseNode excelFileNode = createFileNode( fileName, descriptor );
+              metaverseBuilder.addNode( excelFileNode );
+
+              metaverseBuilder.addLink( excelFileNode, DictionaryConst.LINK_READBY, node );
+            } catch ( MetaverseException e ) {
+              log.error( e.getMessage(), e );
+            }
+          }
+        }
+      }
+    }
+
+    return node;
+  }
+
+  @Override
+  public Set<Class<? extends BaseStepMeta>> getSupportedSteps() {
+    return new HashSet<Class<? extends BaseStepMeta>>() {
+      {
+        add( ExcelInputMeta.class );
+      }
+    };
+  }
+}
---
+++ src/test/java/com/pentaho/metaverse/analyzer/kettle/step/excelinput/ExcelInputStepAnalyzerTest.java
@@ -0,0 +1,323 @@
+/*
+ * PENTAHO CORPORATION PROPRIETARY AND CONFIDENTIAL
+ *
+ * Copyright 2002 - 2015 Pentaho Corporation (Pentaho). All rights reserved.
+ *
+ * NOTICE: All information including source code contained herein is, and
+ * remains the sole property of Pentaho and its licensors. The intellectual
+ * and technical concepts contained herein are proprietary and confidential
+ * to, and are trade secrets of Pentaho and may be covered by U.S. and foreign
+ * patents, or patents in process, and are protected by trade secret and
+ * copyright laws. The receipt or possession of this source code and/or related
+ * information does not convey or imply any rights to reproduce, disclose or
+ * distribute its contents, or to manufacture, use, or sell anything that it
+ * may describe, in whole or in part. Any reproduction, modification, distribution,
+ * or public display of this information without the express written authorization
+ * from Pentaho is strictly prohibited and in violation of applicable laws and
+ * international treaties. Access to the source code contained herein is strictly
+ * prohibited to anyone except those individuals and entities who have executed
+ * confidentiality and non-disclosure agreements or other agreements with Pentaho,
+ * explicitly covering such access.
+ */
+
+package com.pentaho.metaverse.analyzer.kettle.step.excelinput;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.core.row.value.ValueMetaString;
+import org.pentaho.di.core.variables.VariableSpace;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.BaseStepMeta;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.steps.excelinput.ExcelInput;
+import org.pentaho.di.trans.steps.excelinput.ExcelInputField;
+import org.pentaho.di.trans.steps.excelinput.ExcelInputMeta;
+import org.pentaho.platform.api.metaverse.IComponentDescriptor;
+import org.pentaho.platform.api.metaverse.IMetaverseBuilder;
+import org.pentaho.platform.api.metaverse.IMetaverseNode;
+import org.pentaho.platform.api.metaverse.IMetaverseObjectFactory;
+import org.pentaho.platform.api.metaverse.INamespace;
+import org.pentaho.platform.api.metaverse.MetaverseAnalyzerException;
+
+import com.pentaho.dictionary.DictionaryConst;
+import com.pentaho.metaverse.api.model.IExternalResourceInfo;
+import com.pentaho.metaverse.impl.MetaverseComponentDescriptor;
+import com.pentaho.metaverse.testutils.MetaverseTestUtils;
+
+@RunWith( MockitoJUnitRunner.class )
+public class ExcelInputStepAnalyzerTest {
+
+  private ExcelInputStepAnalyzer excelInputStepAnalyzer;
+
+  @Mock
+  private ExcelInput mockExcelInput;
+
+  @Mock
+  private ExcelInputMeta mockExcelInputMeta;
+
+  @Mock
+  private TransMeta mockTransMeta;
+
+  @Mock
+  private RowMetaInterface mockRowMetaInterface;
+
+  @Mock
+  private IMetaverseBuilder mockBuilder;
+
+  @Mock
+  private INamespace mockNamespace;
+
+  private IMetaverseObjectFactory mockFactory;
+
+  private IComponentDescriptor descriptor;
+
+  @Before
+  public void setUp() throws Exception {
+
+    mockFactory = MetaverseTestUtils.getMetaverseObjectFactory();
+    when( mockBuilder.getMetaverseObjectFactory() ).thenReturn( mockFactory );
+    when( mockNamespace.getParentNamespace() ).thenReturn( mockNamespace );
+
+    excelInputStepAnalyzer = new ExcelInputStepAnalyzer();
+    excelInputStepAnalyzer.setMetaverseBuilder( mockBuilder );
+    descriptor = new MetaverseComponentDescriptor( "test", DictionaryConst.NODE_TYPE_JOB, mockNamespace );
+
+    when( mockExcelInput.getStepMetaInterface() ).thenReturn( mockExcelInputMeta );
+  }
+
+  @Test( expected = MetaverseAnalyzerException.class )
+  public void testAnalyze_nullInput() throws Exception {
+    excelInputStepAnalyzer.analyze( null, null );
+  }
+
+  @Test
+  public void testAnalyze_noFields() throws Exception {
+
+    StepMeta meta = new StepMeta( "test", mockExcelInputMeta );
+    StepMeta spyMeta = spy( meta );
+
+    String[] fileNames = new String[]{ "MyExcelInput.txt" };
+
+    when( mockTransMeta.environmentSubstitute( any( String[].class ) ) ).thenReturn( fileNames );
+    when( mockExcelInputMeta.getParentStepMeta() ).thenReturn( spyMeta );
+    when( spyMeta.getParentTransMeta() ).thenReturn( mockTransMeta );
+    when( mockExcelInputMeta.getFileName() ).thenReturn( fileNames );
+
+    IMetaverseNode result = excelInputStepAnalyzer.analyze( descriptor, mockExcelInputMeta );
+    assertNotNull( result );
+    assertEquals( meta.getName(), result.getName() );
+
+    verify( mockExcelInputMeta, times( 1 ) ).getFileName();
+
+    // make sure the step node is added as well as the file node
+    verify( mockBuilder, times( 2 ) ).addNode( any( IMetaverseNode.class ) );
+
+    // make sure there is a "readby" link added
+    verify( mockBuilder, times( 1 ) ).addLink(
+      any( IMetaverseNode.class ), eq( DictionaryConst.LINK_READBY ), any( IMetaverseNode.class ) );
+
+  }
+
+  @Test
+  public void testAnalyze_Fields() throws Exception {
+
+    StepMeta meta = new StepMeta( "test", mockExcelInputMeta );
+    StepMeta spyMeta = spy( meta );
+
+    String[] fileNames = new String[]{ "MyExcelInput.txt" };
+
+    when( mockTransMeta.environmentSubstitute( any( String[].class ) ) ).thenReturn( fileNames );
+    when( mockExcelInputMeta.getParentStepMeta() ).thenReturn( spyMeta );
+    when( spyMeta.getParentTransMeta() ).thenReturn( mockTransMeta );
+    when( mockExcelInputMeta.getFileName() ).thenReturn( fileNames );
+
+    // set up the input fields
+    ExcelInputField field1 = new ExcelInputField( "id", 0, 4 );
+    ExcelInputField field2 = new ExcelInputField( "name", 1, 30 );
+    ExcelInputField[] inputFields = new ExcelInputField[]{ field1, field2 };
+
+    when( mockExcelInputMeta.getField() ).thenReturn( inputFields );
+    when( mockTransMeta.getStepFields( spyMeta ) ).thenReturn( mockRowMetaInterface );
+    when( mockRowMetaInterface.getFieldNames() ).thenReturn( new String[]{ "id", "name" } );
+    when( mockRowMetaInterface.searchValueMeta( Mockito.anyString() ) ).thenAnswer( new Answer<ValueMetaInterface>() {
+
+      @Override
+      public ValueMetaInterface answer( InvocationOnMock invocation ) throws Throwable {
+        Object[] args = invocation.getArguments();
+        if ( args[0] == "id" ) {
+          return new ValueMetaString( "id" );
+        }
+        if ( args[0] == "name" ) {
+          return new ValueMetaString( "name" );
+        }
+        return null;
+      }
+    } );
+
+    IMetaverseNode result = excelInputStepAnalyzer.analyze( descriptor, mockExcelInputMeta );
+    assertNotNull( result );
+    assertEquals( meta.getName(), result.getName() );
+
+    verify( mockExcelInputMeta, times( 1 ) ).getFileName();
+
+    // make sure the step node, the file node, and the field nodes
+    verify( mockBuilder, times( 2 + inputFields.length ) ).addNode( any( IMetaverseNode.class ) );
+
+    // make sure there are "readby" and "uses" links added (file, and each field)
+    verify( mockBuilder, times( 1 ) ).addLink(
+      any( IMetaverseNode.class ), eq( DictionaryConst.LINK_READBY ), any( IMetaverseNode.class ) );
+    verify( mockBuilder, times( inputFields.length ) ).addLink(
+      any( IMetaverseNode.class ), eq( DictionaryConst.LINK_USES ), any( IMetaverseNode.class ) );
+
+    // we should have "populates" links from input nodes to output nodes
+    verify( mockBuilder, times( inputFields.length ) )
+      .addLink( any( IMetaverseNode.class ), eq( DictionaryConst.LINK_POPULATES ), any( IMetaverseNode.class ) );
+
+  }
+
+  @Test
+  public void testAnalyze_FilenamesFromField() throws Exception {
+
+    StepMeta meta = new StepMeta( "test", mockExcelInputMeta );
+    StepMeta spyMeta = spy( meta );
+
+    when( mockExcelInputMeta.getParentStepMeta() ).thenReturn( spyMeta );
+    when( spyMeta.getParentTransMeta() ).thenReturn( mockTransMeta );
+    when( mockExcelInputMeta.getFileName() ).thenReturn( null );
+    when( mockExcelInputMeta.isAcceptingFilenames() ).thenReturn( true );
+    when( mockExcelInputMeta.getAcceptingField() ).thenReturn( "inField" );
+
+    // set up the input fields
+    ExcelInputField field1 = new ExcelInputField( "id", 0, 4 );
+    ExcelInputField field2 = new ExcelInputField( "name", 1, 30 );
+    ExcelInputField[] inputFields = new ExcelInputField[]{ field1, field2 };
+
+    when( mockExcelInputMeta.getField() ).thenReturn( inputFields );
+    when( mockTransMeta.getStepFields( spyMeta ) ).thenReturn( mockRowMetaInterface );
+    when( mockRowMetaInterface.getFieldNames() ).thenReturn( new String[]{ "id", "name" } );
+    when( mockRowMetaInterface.searchValueMeta( Mockito.anyString() ) ).thenAnswer( new Answer<ValueMetaInterface>() {
+
+      @Override
+      public ValueMetaInterface answer( InvocationOnMock invocation ) throws Throwable {
+        Object[] args = invocation.getArguments();
+        if ( args[0] == "id" ) {
+          return new ValueMetaString( "id" );
+        }
+        if ( args[0] == "name" ) {
+          return new ValueMetaString( "name" );
+        }
+        return null;
+      }
+    } );
+
+    RowMetaInterface mockPrevRowMeta = mock( RowMetaInterface.class );
+    when( mockPrevRowMeta.getFieldNames() ).thenReturn( new String[]{ "inField" } );
+    when( mockPrevRowMeta.searchValueMeta( Mockito.anyString() ) ).thenAnswer( new Answer<ValueMetaInterface>() {
+
+      @Override
+      public ValueMetaInterface answer( InvocationOnMock invocation ) throws Throwable {
+        Object[] args = invocation.getArguments();
+        if ( args[0] == "inField" ) {
+          return new ValueMetaString( "inField" );
+        }
+        return null;
+      }
+    } );
+    when( mockTransMeta.getPrevStepFields( spyMeta ) ).thenReturn( mockPrevRowMeta );
+
+    IMetaverseNode result = excelInputStepAnalyzer.analyze( descriptor, mockExcelInputMeta );
+    assertNotNull( result );
+    assertEquals( meta.getName(), result.getName() );
+
+    verify( mockExcelInputMeta, never() ).getFileName();
+    verify( mockExcelInputMeta, times( 1 ) ).isAcceptingFilenames();
+    verify( mockExcelInputMeta, times( 1 ) ).getAcceptingField();
+
+    // make sure the step node and the field nodes have been added, but NOT the incoming stream field
+    verify( mockBuilder, times( 1 + inputFields.length ) ).addNode( any( IMetaverseNode.class ) );
+
+    // Verify there are no READBY links (usually from file to step)
+    verify( mockBuilder, never() ).addLink(
+      any( IMetaverseNode.class ), eq( DictionaryConst.LINK_READBY ), any( IMetaverseNode.class ) );
+
+    // make sure there "uses" links added (each field, and the filename stream field)
+    verify( mockBuilder, times( inputFields.length + 1 ) ).addLink(
+      any( IMetaverseNode.class ), eq( DictionaryConst.LINK_USES ), any( IMetaverseNode.class ) );
+
+    // we should have "populates" links from input nodes to output nodes
+    verify( mockBuilder, times( inputFields.length ) )
+      .addLink( any( IMetaverseNode.class ), eq( DictionaryConst.LINK_POPULATES ), any( IMetaverseNode.class ) );
+
+  }
+
+  @Test
+  public void testGetSupportedSteps() {
+    ExcelInputStepAnalyzer analyzer = new ExcelInputStepAnalyzer();
+    Set<Class<? extends BaseStepMeta>> types = analyzer.getSupportedSteps();
+    assertNotNull( types );
+    assertEquals( types.size(), 1 );
+    assertTrue( types.contains( ExcelInputMeta.class ) );
+  }
+
+  @Test
+  public void testExcelInputExternalResourceConsumer() throws Exception {
+    ExcelInputExternalResourceConsumer consumer = new ExcelInputExternalResourceConsumer();
+
+    StepMeta meta = new StepMeta( "test", mockExcelInputMeta );
+    StepMeta spyMeta = spy( meta );
+
+    when( mockExcelInputMeta.getParentStepMeta() ).thenReturn( spyMeta );
+    when( spyMeta.getParentTransMeta() ).thenReturn( mockTransMeta );
+    when( mockExcelInputMeta.getFileName() ).thenReturn( null );
+    when( mockExcelInputMeta.isAcceptingFilenames() ).thenReturn( false );
+    String[] filePaths = { "/path/to/file1", "/another/path/to/file2" };
+    when( mockExcelInputMeta.getFilePaths( Mockito.any( VariableSpace.class ) ) ).thenReturn( filePaths );
+
+    assertFalse( consumer.isDataDriven( mockExcelInputMeta ) );
+    Collection<IExternalResourceInfo> resources = consumer.getResourcesFromMeta( mockExcelInputMeta );
+    assertFalse( resources.isEmpty() );
+    assertEquals( 2, resources.size() );
+
+
+    when( mockExcelInputMeta.isAcceptingFilenames() ).thenReturn( true );
+    assertTrue( consumer.isDataDriven( mockExcelInputMeta ) );
+    assertTrue( consumer.getResourcesFromMeta( mockExcelInputMeta ).isEmpty() );
+    when( mockRowMetaInterface.getString( Mockito.any( Object[].class ), Mockito.anyString(), Mockito.anyString() ) )
+      .thenReturn( "/path/to/row/file" );
+    resources = consumer.getResourcesFromRow( mockExcelInput, mockRowMetaInterface, new String[]{ "id", "name" } );
+    assertFalse( resources.isEmpty() );
+    assertEquals( 1, resources.size() );
+
+    when( mockRowMetaInterface.getString( Mockito.any( Object[].class ), Mockito.anyString(), Mockito.anyString() ) )
+      .thenThrow( KettleException.class );
+    resources = consumer.getResourcesFromRow( mockExcelInput, mockRowMetaInterface, new String[]{ "id", "name" } );
+    assertTrue( resources.isEmpty() );
+
+    assertEquals( ExcelInputMeta.class, consumer.getMetaClass() );
+  }
+}
