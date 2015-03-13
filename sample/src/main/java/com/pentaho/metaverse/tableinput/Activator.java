392c1fcdcffb86428b427d3107773322929f2a66
Rob Fellows <rfellows@pentaho.com>
2015-03-13 16:37:32 -0400

    [BACKLOG-2386] - Adding a sample sub-module that demonstrates how to contribute a StepAnalyzer and an ExternalResourceConsumer from a separate OSGi bundle.

---
+++ sample/src/main/java/com/pentaho/metaverse/tableinput/Activator.java
@@ -0,0 +1,16 @@
+package com.pentaho.metaverse.tableinput;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+  public void start( BundleContext context ) {
+    System.out.println( "Starting the bundle" );
+  }
+
+  public void stop( BundleContext context ) {
+    System.out.println( "Stopping the bundle" );
+  }
+
+}
