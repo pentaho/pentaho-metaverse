860653e56367019de5fd8331eaaf16c3c7e4f105
Peter Rinehart <peter.rinehart@hitachivantara.com>
2019-02-11 12:46:32 -0500

    [BACKLOG-26935] Updates to annotation analyzer to accomodate subtrans linking and streaming consumer resource input.

--- api/src/main/java/org/pentaho/metaverse/api/analyzer/kettle/annotations/AnnotationDrivenStepMetaAnalyzer.java
+++ api/src/main/java/org/pentaho/metaverse/api/analyzer/kettle/annotations/AnnotationDrivenStepMetaAnalyzer.java
@@ -22,18 +22,31 @@
 
 package org.pentaho.metaverse.api.analyzer.kettle.annotations;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.tinkerpop.blueprints.Direction;
+import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.row.RowMeta;
 import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.core.row.value.ValueMetaNone;
+import org.pentaho.di.trans.ISubTransAwareMeta;
+import org.pentaho.di.trans.StepWithMappingMeta;
+import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.BaseStepMeta;
+import org.pentaho.di.trans.steps.transexecutor.TransExecutorMeta;
+import org.pentaho.di.trans.streaming.common.BaseStreamStepMeta;
 import org.pentaho.dictionary.DictionaryHelper;
 import org.pentaho.metaverse.api.IMetaverseNode;
 import org.pentaho.metaverse.api.MetaverseAnalyzerException;
 import org.pentaho.metaverse.api.MetaverseComponentDescriptor;
 import org.pentaho.metaverse.api.StepField;
+import org.pentaho.metaverse.api.analyzer.kettle.KettleAnalyzerUtil;
+import org.pentaho.metaverse.api.analyzer.kettle.step.IClonableStepAnalyzer;
 import org.pentaho.metaverse.api.analyzer.kettle.step.StepAnalyzer;
 import org.pentaho.metaverse.api.analyzer.kettle.step.StepNodes;
+import org.pentaho.metaverse.api.analyzer.kettle.step.SubtransAnalyzer;
+import org.pentaho.metaverse.api.messages.Messages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.List;
@@ -47,6 +60,8 @@ import static java.util.Arrays.asList;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toMap;
 import static java.util.stream.Collectors.toSet;
+import static org.pentaho.dictionary.DictionaryConst.LINK_OUTPUTS;
+import static org.pentaho.dictionary.DictionaryConst.PROPERTY_TYPE;
 import static org.pentaho.metaverse.api.analyzer.kettle.step.ExternalResourceStepAnalyzer.RESOURCE;
 
 /**
@@ -55,11 +70,14 @@ import static org.pentaho.metaverse.api.analyzer.kettle.step.ExternalResourceSte
  * See README.md
  */
 public class AnnotationDrivenStepMetaAnalyzer extends StepAnalyzer<BaseStepMeta> {
+  private final transient Logger log = LoggerFactory.getLogger( AnnotationDrivenStepMetaAnalyzer.class );
+
   private final transient Class<? extends BaseStepMeta> stepClass;
   private final AtomicBoolean streamFieldsLoaded = new AtomicBoolean( false );
 
   private final Map<String, String> typeCategoryMap;
   private final transient EntityRegister register;
+  private final transient BaseStepMeta meta;
 
   @SuppressWarnings ( "unused" )
   public AnnotationDrivenStepMetaAnalyzer( BaseStepMeta meta ) {
@@ -67,10 +85,16 @@ public class AnnotationDrivenStepMetaAnalyzer extends StepAnalyzer<BaseStepMeta>
   }
 
   AnnotationDrivenStepMetaAnalyzer( BaseStepMeta meta, Map<String, String> typeCategoryMap, EntityRegister register ) {
-    stepClass = meta.getClass();
+    this.meta = meta;
+    this.stepClass = meta.getClass();
     this.typeCategoryMap = typeCategoryMap;
     this.register = register;
     registerTypes();
+  }
+
+  @Override
+  protected IClonableStepAnalyzer newInstance() {
+    return new AnnotationDrivenStepMetaAnalyzer( this.meta );
   }
 
   /**
@@ -98,6 +122,79 @@ public class AnnotationDrivenStepMetaAnalyzer extends StepAnalyzer<BaseStepMeta>
     rootNode.setProperties(
       annoFields.props()
         .collect( toMap( AnnotatedClassField::name, AnnotatedClassField::val ) ) );
+
+    // check for a subtrans and generate the graph
+    if ( meta instanceof StepWithMappingMeta ) {
+      analyzeAndLinkSubtrans( meta, rootNode, externalNodes );
+    }
+  }
+
+  private void analyzeAndLinkSubtrans( Object meta, IMetaverseNode rootNode,
+                                       Map<AnnotatedClassField<Metaverse.Node>, IMetaverseNode> externalNodes )
+    throws MetaverseAnalyzerException {
+    try {
+      // analyze subtrans and generate lineage graph
+      TransMeta subTransMeta = TransExecutorMeta
+        .loadMappingMeta( (StepWithMappingMeta) meta, parentTransMeta.getRepository(), parentTransMeta.getMetaStore(),
+          parentTransMeta );
+
+      IMetaverseNode subTransNode =
+        KettleAnalyzerUtil.analyze( this, parentTransMeta, (ISubTransAwareMeta) meta, rootNode );
+
+      SubtransAnalyzer<BaseStepMeta> subtransAnalyzer = getSubtransAnalyzer();
+
+      linkToSubTransInputs( externalNodes, subTransMeta, subTransNode, subtransAnalyzer );
+
+      String resultFieldsStep = getSubTransResultFieldName();
+
+      if ( null != resultFieldsStep && !resultFieldsStep.equals( "" ) ) {
+        linkToSubTransOutputs( subTransMeta, subTransNode, subtransAnalyzer, resultFieldsStep );
+      }  else {
+        log.warn( Messages.getString( "ERROR.AnnotationDrivenStepMetaAnalyzer.SubtransResultNotFound" ) );
+      }
+
+    } catch ( KettleException e ) {
+      log.error(
+        Messages.getErrorString( "WARN.AnnotationDrivenStepMetaAnalyzer.SubtransProcessingException" ), e );
+    }
+  }
+
+  private void linkToSubTransOutputs( TransMeta subTransMeta, IMetaverseNode subTransNode,
+                                      SubtransAnalyzer<BaseStepMeta> subtransAnalyzer, String resultFieldsStep ) {
+    StepNodes outputNodes = this.getOutputs();
+    for ( String stepName : outputNodes.getStepNames() ) {
+      // for each step after the consumer
+      for ( String fieldName : outputNodes.getFieldNames( stepName ) ) {
+        // for each field going to that step, link it to the field returned from the subtrans
+        subtransAnalyzer.linkResultFieldToSubTrans( outputNodes.findNode( stepName, fieldName ), subTransMeta,
+          subTransNode, descriptor, resultFieldsStep );
+      }
+    }
+  }
+
+  private void linkToSubTransInputs( Map<AnnotatedClassField<Metaverse.Node>, IMetaverseNode> externalNodes,
+                                     TransMeta subTransMeta, IMetaverseNode subTransNode,
+                                     SubtransAnalyzer<BaseStepMeta> subtransAnalyzer ) {
+    externalNodes.entrySet().stream()
+      .filter( entry -> entry.getKey().annotation.subTransLink().equals( Metaverse.SUBTRANS_INPUT ) )
+      .forEach( entry ->
+        subtransAnalyzer.linkUsedFieldToSubTrans( entry.getValue(), subTransMeta, subTransNode, descriptor,
+          fieldName -> fieldName.equals( entry.getKey().val() ) ) );
+  }
+
+
+  protected String getSubTransResultFieldName() {
+    String resultFieldsStep = "";
+    if ( baseStepMeta instanceof BaseStreamStepMeta ) {
+      resultFieldsStep = ( (BaseStreamStepMeta) baseStepMeta ).getSubStep();
+    } else if ( baseStepMeta instanceof TransExecutorMeta ) {
+      if ( ( (TransExecutorMeta) baseStepMeta ).getOutputRowsSourceStep() != null ) {
+        resultFieldsStep = ( (TransExecutorMeta) baseStepMeta ).getOutputRowsSourceStep();
+      } else if ( ( (TransExecutorMeta) baseStepMeta ).getExecutorsOutputStep() != null ) {
+        resultFieldsStep = ( (TransExecutorMeta) baseStepMeta ).getExecutorsOutputStep();
+      }
+    }
+    return resultFieldsStep;
   }
 
   @Override
@@ -124,30 +221,18 @@ public class AnnotationDrivenStepMetaAnalyzer extends StepAnalyzer<BaseStepMeta>
   @Override protected Map<String, RowMetaInterface> getOutputRowMetaInterfaces( BaseStepMeta meta ) {
     Map<String, RowMetaInterface> rowMetas = super.getOutputRowMetaInterfaces( meta );
 
+    AnnotatedClassFields nodeTree = new AnnotatedClassFields( meta );
+
     RowMeta resourceRowMeta = new RowMeta();
     new AnnotatedClassFields( meta )
       .links()
+      .filter( field -> nodeTree.node( field.annotation.nodeName() ).get().annotation.link().equals( LINK_OUTPUTS ) )
       .forEach( field -> resourceRowMeta.addValueMeta( new ValueMetaNone( field.name ) ) );
 
     if ( resourceRowMeta.size() > 0 ) {
       rowMetas.put( RESOURCE, resourceRowMeta );
     }
     return rowMetas;
-  }
-
-  /**
-   * output rowmeta limited to the "used" fields
-   */
-  @Override public RowMetaInterface getOutputFields( BaseStepMeta meta ) {
-    RowMetaInterface rowMeta = super.getOutputFields( meta );
-    RowMetaInterface usedRowMeta = new RowMeta();
-    getUsedFields( meta ).stream()
-      .map( StepField::getFieldName )
-      .map( rowMeta::indexOfValue )
-      .filter( index -> index >= 0 )
-      .forEach( index -> usedRowMeta.addValueMeta( rowMeta.getValueMeta( index ) ) );
-
-    return usedRowMeta;
   }
 
   /**
@@ -168,15 +253,38 @@ public class AnnotationDrivenStepMetaAnalyzer extends StepAnalyzer<BaseStepMeta>
   private void linkResourceFieldToNode( Map<AnnotatedClassField<Metaverse.Node>, IMetaverseNode> resourceNodes,
                                         Metaverse.NodeLink nodeLink,
                                         AnnotatedClassFields annoFields ) {
-    StepNodes stepNodes = isOutLink( nodeLink ) ? getOutputs() : getInputs();
-    if ( stepNodes == null ) {
-      return;  // nothing to link
-    }
-    IMetaverseNode resourceFieldNode = stepNodes.findNode( RESOURCE, nodeLink.nodeName() );
+    IMetaverseNode resourceFieldNode = null;
 
-    annoFields.node( nodeLink.parentNodeName() )
-      .map( resourceNodes::get )
-      .ifPresent( parentNode -> addLink( nodeLink, resourceFieldNode, parentNode ) );
+    // see if the link is pointing to an input/output field node
+    StepNodes stepNodes = isOutLink( nodeLink ) ? getOutputs() : getInputs();
+    if ( stepNodes != null && !stepNodes.getFieldNames().isEmpty() ) {
+      resourceFieldNode = stepNodes.findNode( RESOURCE, nodeLink.nodeName() );
+    }
+
+    if ( resourceFieldNode == null ) {
+      // link is probably to a resource node that isn't an input or output field
+      final StepNodes newStepNodeObj = new StepNodes();
+      resourceNodes.entrySet().stream()
+        .filter( mapEntry -> mapEntry.getValue().getProperty( PROPERTY_TYPE ).equals( RESOURCE ) )
+        .forEach( mapEntry -> newStepNodeObj.addNode( RESOURCE, mapEntry.getValue().getName(), mapEntry.getValue() ) );
+      stepNodes = newStepNodeObj;
+
+      resourceFieldNode = stepNodes.findNode( RESOURCE, nodeLink.nodeName() );
+    }
+
+    IMetaverseNode childNode = resourceFieldNode;
+
+    if ( childNode != null ) {
+      annoFields.node( nodeLink.parentNodeName() )
+        .map( resourceNodes::get )
+        .ifPresent( parentNode -> addLink( nodeLink, childNode, parentNode ) );
+    } else {
+      String parentNodeLink = nodeLink.parentNodelink();
+      String fromNode = nodeLink.nodeName();
+      String toNode = nodeLink.parentNodeName();
+      log.warn( Messages.getErrorString( "ERROR.AnnotationDrivenStepMetaAnalyzer.LinkError",
+        parentNodeLink, fromNode, toNode ) );
+    }
   }
 
   /**
@@ -246,6 +354,11 @@ public class AnnotationDrivenStepMetaAnalyzer extends StepAnalyzer<BaseStepMeta>
     return createNodeFromDescriptor(
       new MetaverseComponentDescriptor(
         field.name, field.annotation.type(), rootNode, getDescriptor().getContext() ) );
+  }
+
+  @VisibleForTesting
+  protected SubtransAnalyzer<BaseStepMeta> getSubtransAnalyzer() {
+    return new SubtransAnalyzer<>( this, log );
   }
 
   /**
