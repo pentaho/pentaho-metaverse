f313f03ba9508be0a86699ee6a7140fb3e307133
Nicholas Jordan <nicholas.jordan@hitachivantara.com>
2020-07-27 09:58:34 -0400

    [CUST-248] Backport of PDI-18657 into 8.3.0.8

--- api/src/main/java/org/pentaho/metaverse/api/IMetaverseConfig.java
+++ api/src/main/java/org/pentaho/metaverse/api/IMetaverseConfig.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2020 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -47,4 +47,8 @@ public interface IMetaverseConfig {
   void setConsolidateSubGraphs( final boolean consolidateGraphs );
 
   boolean getConsolidateSubGraphs();
+
+  void setExternalResourceCacheExpireTime( final String cacheExpire );
+
+  String getExternalResourceCacheExpireTime();
 }
--- api/src/test/java/org/pentaho/metaverse/api/analyzer/kettle/ExternalResourceCacheTest.java
+++ api/src/test/java/org/pentaho/metaverse/api/analyzer/kettle/ExternalResourceCacheTest.java
@@ -22,7 +22,6 @@
 
 package org.pentaho.metaverse.api.analyzer.kettle;
 
-import org.apache.commons.collections.IteratorUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,17 +32,21 @@ import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.StepMeta;
 import org.pentaho.di.trans.steps.file.BaseFileInputMeta;
+import org.pentaho.metaverse.api.IMetaverseConfig;
 import org.pentaho.metaverse.api.model.IExternalResourceInfo;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
 import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 @RunWith( PowerMockRunner.class )
 public class ExternalResourceCacheTest {
@@ -62,169 +65,114 @@ public class ExternalResourceCacheTest {
   private TransMeta transMeta;
 
   @Mock
-  private BaseFileInputMeta meta;
+  private BaseFileInputMeta meta1;
 
-  private StepMeta spyMeta;
+  @Mock
+  private BaseFileInputMeta meta2;
 
-  @Before
-  public void setup() {
-    ExternalResourceCache.getInstance().transMap = new ConcurrentHashMap();
-    ExternalResourceCache.getInstance().resourceMap = new ConcurrentHashMap();
-  }
+  private StepMeta spyMeta1;
+
+  private StepMeta spyMeta2;
+
+  private ExternalResourceCache testInstance;
 
   private void initMetas() {
-    spyMeta = spy( new StepMeta( "test", meta ) );
-    when( spyMeta.getParentTransMeta() ).thenReturn( transMeta );
-    when( meta.getParentStepMeta() ).thenReturn( spyMeta );
+    spyMeta1 = spy( new StepMeta( "test", meta1) );
+    when( spyMeta1.getParentTransMeta() ).thenReturn( transMeta );
+    when( meta1.getParentStepMeta() ).thenReturn(spyMeta1);
     when( transMeta.getFilename() ).thenReturn( "my_file" );
+
+    spyMeta2 = spy( new StepMeta( "test2", meta2) );
+    when( spyMeta2.getParentTransMeta() ).thenReturn( transMeta2 );
+    when( meta2.getParentStepMeta() ).thenReturn(spyMeta2);
+    when( transMeta2.getFilename() ).thenReturn( "my_file2" );
 
     when( trans.getName() ).thenReturn( "my_trans" );
     when( trans.getFilename() ).thenReturn( "my_file" );
 
     when( trans.getTransMeta() ).thenReturn( transMeta );
-    when( transMeta.getSteps() ).thenReturn( Arrays.asList( new StepMeta[] { spyMeta } ) );
+    when( transMeta.getSteps() ).thenReturn( Arrays.asList( new StepMeta[] {spyMeta1} ) );
 
     when( trans2.getTransMeta() ).thenReturn( transMeta2 );
     when( trans2.getName() ).thenReturn( "my_trans2" );
     when( trans2.getFilename() ).thenReturn( "my_file2" );
-    when( transMeta2.getSteps() ).thenReturn( Arrays.asList( new StepMeta[] { spyMeta } ) );
+    when( transMeta2.getSteps() ).thenReturn( Arrays.asList( new StepMeta[] {spyMeta2} ) );
+  }
+
+  @Before
+  public void setup() {
+    testInstance = new ExternalResourceCache(null );
   }
 
   @Test
   public void test_getInstance() {
+    assertNotNull( ExternalResourceCache.getInstance() );
     // verify that we have a singleton
     assertEquals( ExternalResourceCache.getInstance(), ExternalResourceCache.getInstance() );
   }
 
   @Test
   public void test_getUniqueId() {
-    assertNull( ExternalResourceCache.getInstance().getUniqueId( null ) );
+    assertNull( testInstance.getUniqueId( null ) );
     // parent TransMeta is not yet mocked, we should get null back
-    assertNull( ExternalResourceCache.getInstance().getUniqueId( meta.getParentStepMeta() ) );
+    assertNull( testInstance.getUniqueId( meta1.getParentStepMeta() ) );
 
     initMetas();
 
     // null repo
     assertEquals( System.getProperty( "user.dir" ) + File.separator + "my_file::test",
-      ExternalResourceCache.getInstance().getUniqueId( meta.getParentStepMeta() ) );
+      testInstance.getUniqueId( meta1.getParentStepMeta() ) );
 
     // mock the repo
     when( transMeta.getRepository() ).thenReturn( Mockito.mock( Repository.class ) );
     assertEquals( "null.null::test",
-      ExternalResourceCache.getInstance().getUniqueId( meta.getParentStepMeta() ) );
+      testInstance.getUniqueId( meta1.getParentStepMeta() ) );
 
     when( transMeta.getPathAndName() ).thenReturn( "path_and_name" );
     when( transMeta.getDefaultExtension() ).thenReturn( "ktr" );
     assertEquals( "path_and_name.ktr::test",
-      ExternalResourceCache.getInstance().getUniqueId( meta.getParentStepMeta() ) );
+      testInstance.getUniqueId( meta1.getParentStepMeta() ) );
   }
 
   @Test
   public void test_caching() {
     initMetas();
 
-    assertNull( ExternalResourceCache.getInstance().get( null, null ) );
-    assertEquals( 0, ExternalResourceCache.getInstance().transMap.size() );
-    assertEquals( 0, ExternalResourceCache.getInstance().resourceMap.size() );
+    assertNull( testInstance.get( null ) );
+    assertEquals( 0, testInstance.resourceCache.size() );
 
-    assertNull( ExternalResourceCache.getInstance().get( trans, null ) );
-    assertEquals( 0, ExternalResourceCache.getInstance().transMap.size() );
-    assertEquals( 0, ExternalResourceCache.getInstance().resourceMap.size() );
+    assertNull( testInstance.get( meta1) );
+    assertEquals( 0, testInstance.resourceCache.size() );
 
-    assertNull( ExternalResourceCache.getInstance().get( trans, meta ) );
-    assertEquals( 1, ExternalResourceCache.getInstance().transMap.size() );
-    assertEquals( 0, ExternalResourceCache.getInstance().resourceMap.size() );
+    assertNull( testInstance.get( meta2) );
+    assertEquals( 0, testInstance.resourceCache.size() );
 
-    // the cache contains sets, make sure another copy of the same trans is not added
-    assertNull( ExternalResourceCache.getInstance().get( trans, meta ) );
-    assertEquals( 1, ExternalResourceCache.getInstance().transMap.size() );
-    List<ExternalResourceCache.TransValues> cachedTransformations = IteratorUtils.toList(
-      ExternalResourceCache.getInstance().transMap.values().iterator() );
-    assertEquals( 1, cachedTransformations.size() );
-    assertEquals( 1, cachedTransformations.get( 0 ).size() );
-    assertTrue( cachedTransformations.get( 0 ).contains( trans ) );
-    assertEquals( 0, ExternalResourceCache.getInstance().resourceMap.size() );
-
-    assertNull( ExternalResourceCache.getInstance().get( trans2, meta ) );
-    assertEquals( 1, ExternalResourceCache.getInstance().transMap.size() );
-    cachedTransformations = IteratorUtils.toList(
-      ExternalResourceCache.getInstance().transMap.values().iterator() );
-    assertEquals( 1, cachedTransformations.size() );
-    assertEquals( 2, cachedTransformations.get( 0 ).size() );
-    assertTrue( cachedTransformations.get( 0 ).contains( trans ) );
-    assertTrue( cachedTransformations.get( 0 ).contains( trans2 ) );
-    assertEquals( 0, ExternalResourceCache.getInstance().resourceMap.size() );
-
-    final ExternalResourceCache.ExternalResourceValues resources = ExternalResourceCache.getInstance()
+    final ExternalResourceCache.ExternalResourceValues resources1 = testInstance
       .newExternalResourceValues();
-    resources.add( Mockito.mock( IExternalResourceInfo.class ) );
+    resources1.add( Mockito.mock( IExternalResourceInfo.class ) );
 
-    // cache some resources
-    ExternalResourceCache.getInstance().cache( trans, meta, resources );
-    assertEquals( 1, ExternalResourceCache.getInstance().transMap.size() );
-    cachedTransformations = IteratorUtils.toList(
-      ExternalResourceCache.getInstance().transMap.values().iterator() );
-    assertEquals( 1, cachedTransformations.size() );
-    assertEquals( 2, cachedTransformations.get( 0 ).size() );
-    assertTrue( cachedTransformations.get( 0 ).contains( trans ) );
-    assertTrue( cachedTransformations.get( 0 ).contains( trans2 ) );
-    assertEquals( 1, ExternalResourceCache.getInstance().resourceMap.size() );
+    final ExternalResourceCache.ExternalResourceValues resources2 = testInstance
+      .newExternalResourceValues();
+    resources2.add( Mockito.mock( IExternalResourceInfo.class ) );
+
+    // cache some resources1
+    testInstance.cache( meta1, resources1 );
+    assertEquals( 1, testInstance.resourceCache.size() );
+
+    // cache some resources2
+    testInstance.cache( meta2, resources2 );
+    assertEquals( 2, testInstance.resourceCache.size() );
 
     // remote trans from the cache
-    ExternalResourceCache.getInstance().removeCachedResources( trans );
-    // trans2 should still exist in the map
-    assertEquals( 1, ExternalResourceCache.getInstance().transMap.size() );
-    cachedTransformations = IteratorUtils.toList(
-      ExternalResourceCache.getInstance().transMap.values().iterator() );
-    assertEquals( 1, cachedTransformations.size() );
-    assertEquals( 1, cachedTransformations.get( 0 ).size() );
-    assertTrue( cachedTransformations.get( 0 ).contains( trans2 ) );
-    // resources should not be removed as they are still being used by trans2
-    assertEquals( 1, ExternalResourceCache.getInstance().resourceMap.size() );
+    testInstance.removeCachedResources( trans );
+    assertEquals( 1, testInstance.resourceCache.size() );
+    assertNull( testInstance.get( meta1 ) );
+    assertEquals( resources2, testInstance.get( meta2 ) );
 
     // remove trans2 from the cache
-    ExternalResourceCache.getInstance().removeCachedResources( trans2 );
-    // transMap and resourceMap should now both be empty
-    assertEquals( 0, ExternalResourceCache.getInstance().transMap.size() );
-    assertEquals( 0, ExternalResourceCache.getInstance().resourceMap.size() );
-  }
-
-  @Test
-  public void test_ExternalResourceTransValues() {
-    final Trans trans1 = new Trans();
-    trans1.setTransMeta( transMeta1 );
-    when( transMeta1.getName() ).thenReturn( "trans1file" );
-    when( transMeta1.getFilename() ).thenReturn( "trans1file.ktr" );
-
-    final Trans trans2 = new Trans();
-    trans2.setTransMeta( transMeta2 );
-    when( transMeta2.getName() ).thenReturn( "trans2file" );
-    when( transMeta2.getFilename() ).thenReturn( "trans2file.ktr" );
-
-    final Trans trans3 = new Trans();
-    trans3.setTransMeta( transMeta1 );
-
-    final ExternalResourceCache.Resources transValues = ExternalResourceCache.getInstance().new TransValues();
-    assertEquals( 0, transValues.size() );
-    transValues.add( trans1 );
-    assertEquals( 1, transValues.size() );
-    assertTrue( transValues.contains( trans1 ) );
-    transValues.add( trans2 );
-    assertEquals( 2, transValues.size() );
-    assertTrue( transValues.contains( trans2 ) );
-    transValues.add( trans3 );
-    assertEquals( 2, transValues.size() );
-    assertFalse( transValues.contains( trans3 ) );
-
-    transValues.remove( trans1 );
-    assertEquals( 1, transValues.size() );
-    assertFalse( transValues.contains( trans1 ) );
-    assertTrue( transValues.contains( trans2 ) );
-    transValues.remove( trans2 );
-    assertEquals( 0, transValues.size() );
-
-    // make sure we're getting a copy, not the original
-    assertFalse( transValues.getInternal() == transValues.getInternal() );
+    testInstance.removeCachedResources( trans2 );
+    assertNull( testInstance.get( meta2 ) );
+    assertEquals( 0, testInstance.resourceCache.size() );
   }
 
   @Test
@@ -250,5 +198,59 @@ public class ExternalResourceCacheTest {
 
     // make sure we're getting a copy, not the original
     assertFalse( resourceValues.getInternal() == resourceValues.getInternal() );
+  }
+
+  @Test
+  public void test_caching_timeout() throws Exception {
+    long timeoutInSeconds = 1;
+    IMetaverseConfig metaverseConfig = mock( IMetaverseConfig.class );
+    when( metaverseConfig.getExternalResourceCacheExpireTime() ).thenReturn( Long.toString( timeoutInSeconds ) );
+    testInstance = new ExternalResourceCache( metaverseConfig );
+
+    initMetas();
+
+    final ExternalResourceCache.ExternalResourceValues resources1 = testInstance
+      .newExternalResourceValues();
+    resources1.add( Mockito.mock( IExternalResourceInfo.class ) );
+
+    final ExternalResourceCache.ExternalResourceValues resources2 = testInstance
+      .newExternalResourceValues();
+    resources2.add( Mockito.mock( IExternalResourceInfo.class ) );
+
+    // verify nothing in cache
+    assertNull( testInstance.get( meta1 ) );
+    assertNull( testInstance.get( meta2 ) );
+
+    // add items
+    testInstance.cache( meta1, resources1 );
+    testInstance.cache( meta2, resources2 );
+    assertEquals( resources1, testInstance.get( meta1 ) );
+    assertEquals( resources2, testInstance.get( meta2 ) );
+
+    // sleep to test time eviction policy
+    Thread.sleep( 11 * timeoutInSeconds * 1000 / 10 ); // eviction policy 1.1  * timeout
+
+    // verify
+    assertNull( testInstance.get( meta1 ) );
+    assertNull( testInstance.get( meta2 ) );
+  }
+
+  @Test
+  public void test_GetCacheExpireTime() throws Exception {
+
+    // TEST1 : config null
+    assertEquals( ExternalResourceCache.DEFAULT_TIMEOUT_SECONDS, testInstance.getCacheExpireTime( null ) );
+
+    // TEST2: config expireTime value is null
+    IMetaverseConfig metaverseConfig1 = mock( IMetaverseConfig.class );
+    when( metaverseConfig1.getExternalResourceCacheExpireTime() ).thenReturn( null );
+    assertEquals( ExternalResourceCache.DEFAULT_TIMEOUT_SECONDS, testInstance.getCacheExpireTime( metaverseConfig1 ) );
+
+    // TEST3:
+    long expireTime = 423L;
+    IMetaverseConfig metaverseConfig2 = mock( IMetaverseConfig.class );
+    when( metaverseConfig2.getExternalResourceCacheExpireTime() ).thenReturn( Long.toString( expireTime ) );
+    assertEquals( expireTime, testInstance.getCacheExpireTime( metaverseConfig2 ) );
+
   }
 }
