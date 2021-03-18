6c9b6db9f803f5e66731686f303693570bc0ad63
Angel Ramos <angel.ramos@hitachivantara.com>
2021-03-18 13:06:25 -0400

    [BACKLOG-35252] Initial implementation of lineage for LDOS/LDC based on prototype from Angel Ramos Cardona.

---
+++ core/src/it/java/org/pentaho/metaverse/graph/GraphCatalogWriterIT.java
@@ -0,0 +1,132 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2021 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.metaverse.graph;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.pentaho.metaverse.graph.catalog.CatalogLineageClient;
+import org.pentaho.metaverse.graph.catalog.FieldLevelRelationship;
+import org.pentaho.metaverse.graph.catalog.LineageDataResource;
+import org.pentaho.metaverse.step.StepAnalyzerValidationIT;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GraphCatalogWriterIT extends StepAnalyzerValidationIT {
+  @Mock CatalogLineageClient mockCatalogLineageClient;
+  @Captor ArgumentCaptor<List<LineageDataResource>> inputSourceCaptor;
+  @Captor ArgumentCaptor<List<LineageDataResource>> outputSourcesCaptor;
+
+  @Test
+  public void testMultiSource() throws Exception {
+    final String transNodeName = "CombineMultiSourceToTarget";
+    initTest( transNodeName );
+
+    GraphCatalogWriter graphCatalogWriter = new GraphCatalogWriter( mockCatalogLineageClient );
+
+    graphCatalogWriter.outputGraphImpl( graph, null );
+
+    LineageDataResource personCsv = new LineageDataResource( "person.csv" );
+    personCsv.setPath( "/Users/aramos/Documents/Hitachi/REPOS/R2D2-DEV/CatalogTestKTR/person.csv" );
+    List<String> personFields = Arrays.asList( "first_name", "id", "last_name" );
+    personCsv.setFields( personFields );
+    LineageDataResource personDetailsCsv = new LineageDataResource( "person_details.csv" );
+    personDetailsCsv.setPath( "/Users/aramos/Documents/Hitachi/REPOS/R2D2-DEV/CatalogTestKTR/person_details.csv" );
+    List<String> personDetailsFields = Arrays.asList( "gender", "ip_address", "id", "age", "email" );
+    personDetailsCsv.setFields( personDetailsFields );
+    LineageDataResource outputTarget = new LineageDataResource( "CombinedCsvToTextOut.csv" );
+    outputTarget.setPath( "/Users/aramos/Documents/Hitachi/REPOS/R2D2-DEV/CatalogTestKTR/out/CombinedCsvToTextOut.csv" );
+    List<String> outputFields = Arrays.asList( "GIVEN", "HOST", "SUR", "SPAN", "USERNAME", "LONG_LEGAL", "SSN", "SEX" );
+    outputTarget.setFields( outputFields );
+    addRelationship( personCsv, outputTarget, "first_name", "GIVEN" );
+    addRelationship( personCsv, outputTarget, "last_name", "SUR" );
+    addRelationship( personCsv, outputTarget, "id", "SSN" );
+    addRelationship( personDetailsCsv, outputTarget, "ip_address", "HOST" );
+    addRelationship( personDetailsCsv, outputTarget, "age", "SPAN" );
+    addRelationship( personDetailsCsv, outputTarget, "email", "USERNAME" );
+    addRelationship( personDetailsCsv, outputTarget, "gender", "SEX" );
+
+    Mockito.verify( mockCatalogLineageClient ).processLineage( inputSourceCaptor.capture(), outputSourcesCaptor.capture() );
+    List<LineageDataResource> inputSources = inputSourceCaptor.getValue();
+    Assert.assertNotNull( "input sources must not be null", inputSources );
+    listContainsExpectedResource( personCsv, inputSources );
+    listContainsExpectedResource( personDetailsCsv, inputSources );
+    List<LineageDataResource> outputSources = outputSourcesCaptor.getValue();
+    Assert.assertNotNull( "output sources must not be null", outputSources );
+    listContainsExpectedResource( outputTarget, outputSources );
+  }
+
+  private void listContainsExpectedResource( LineageDataResource resource, List<LineageDataResource> sourceList ) {
+    Assert.assertTrue( String.format( "did not find %s resource", resource.getName() ), sourceList.contains( resource ) );
+    List<FieldLevelRelationship> personCsvRelationshipsComputed = sourceList.stream().filter( r -> r.getName().equals( resource.getName() ) ).findFirst().get()
+      .getFieldLevelRelationships();
+    Assert.assertTrue( String.format( "field relationships in %s resource incorrect", resource.getName() ),
+      personCsvRelationshipsComputed.containsAll( resource.getFieldLevelRelationships() )
+        && ( personCsvRelationshipsComputed.size() == resource.getFieldLevelRelationships().size() ) );
+  }
+
+  @Test
+  public void testFileToDb() throws Exception {
+    final String transNodeName = "file_to_db";
+    initTest( transNodeName );
+
+    GraphCatalogWriter graphCatalogWriter = new GraphCatalogWriter( mockCatalogLineageClient );
+
+    graphCatalogWriter.outputGraphImpl( graph, null );
+
+    LineageDataResource personCsv = new LineageDataResource( "person.csv" );
+    personCsv.setPath( "/mnt/c/Users/prinehart/Downloads/person.csv" );
+    List<String> personFields = Arrays.asList( "first_name", "id", "last_name" );
+    personCsv.setFields( personFields );
+    LineageDataResource outputTarget = new LineageDataResource( "silly_test" );
+    outputTarget.setDbName( "hibernate" );
+    outputTarget.setDbSchema( "public" );
+    outputTarget.setDbHost( "192.168.50.68" );
+    List<String> outputFields = Arrays.asList( "SSN", "NAME1", "NAME2" );
+    outputTarget.setFields( outputFields );
+    addRelationship( personCsv, outputTarget, "first_name", "NAME1" );
+    addRelationship( personCsv, outputTarget, "last_name", "NAME2" );
+    addRelationship( personCsv, outputTarget, "id", "SSN" );
+
+    Mockito.verify( mockCatalogLineageClient ).processLineage( inputSourceCaptor.capture(), outputSourcesCaptor.capture() );
+    List<LineageDataResource> inputSources = inputSourceCaptor.getValue();
+    listContainsExpectedResource( personCsv, inputSources );
+    List<LineageDataResource> outputSources = outputSourcesCaptor.getValue();
+    Assert.assertNotNull( "output sources must not be null", outputSources );
+    listContainsExpectedResource( outputTarget, outputSources );
+  }
+
+  private void addRelationship( LineageDataResource input, LineageDataResource output, String inputField, String outputField ) {
+    FieldLevelRelationship r1 = new FieldLevelRelationship();
+    r1.setInputSourceResource( input );
+    r1.setOutputTargetResource( output );
+    r1.setInputSourceResourceField( inputField );
+    r1.setOutputTargetResourceField( outputField );
+    input.addFieldLevelRelationship( r1 );
+    output.addFieldLevelRelationship( r1 );
+  }
+}
---
+++ core/src/main/java/org/pentaho/metaverse/graph/catalog/LineageDataResource.java
@@ -0,0 +1,170 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2021 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+
+package org.pentaho.metaverse.graph.catalog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.pentaho.metaverse.util.MetaverseUtil.safeListMatch;
+import static org.pentaho.metaverse.util.MetaverseUtil.safeStringMatch;
+
+public class LineageDataResource {
+
+  private final String name;
+  private String path;
+  private List<String> fields;
+  private final List<FieldLevelRelationship> fieldRelationships = new ArrayList<>();
+  private String catalogResourceID;
+  private Object vertexId;
+  private String dbSchema;
+  private String dbHost;
+  private String dbName;
+  private String dbPort;
+
+  public LineageDataResource(String name ) {
+    this.name = name;
+    path = "";
+    catalogResourceID = "";
+    vertexId = null;
+    dbSchema = "";
+    dbHost = "";
+    dbName = "";
+    dbPort = "";
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<String> getFields() {
+    return fields;
+  }
+
+  public void setFields( List<String> fields ) {
+    this.fields = fields;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public void setPath( String path ) {
+    this.path = path;
+  }
+
+  public List<FieldLevelRelationship> getFieldLevelRelationships() {
+    return fieldRelationships;
+  }
+
+  public void addFieldLevelRelationship( FieldLevelRelationship fieldRelationship ) {
+    fieldRelationships.add( fieldRelationship );
+  }
+
+  public void setCatalogResourceID( String catalogResourceID ) {
+    this.catalogResourceID = catalogResourceID;
+  }
+
+  public String getCatalogResourceID() {
+    return catalogResourceID;
+  }
+
+  public Object getVertexId() {
+    return vertexId;
+  }
+
+  public void setVertexId( Object vertexId ) {
+    this.vertexId = vertexId;
+  }
+
+  public String getDbSchema() {
+    return dbSchema;
+  }
+
+  public void setDbSchema( String dbSchema ) {
+    this.dbSchema = dbSchema;
+  }
+
+  public String getDbHost() {
+    return dbHost;
+  }
+
+  public void setDbHost( String dbHost ) {
+    this.dbHost = dbHost;
+  }
+
+  public String getDbName() {
+    return dbName;
+  }
+
+  public void setDbName( String dbName ) {
+    this.dbName = dbName;
+  }
+
+  public String getDbPort() {
+    return dbPort;
+  }
+
+  public void setDbPort( String dbPort ) {
+    this.dbPort = dbPort;
+  }
+
+  @Override
+  public String toString() {
+    return "name: ".concat( name )
+      .concat( " path: " ).concat( path )
+      .concat( " catalogResourceId: " ).concat( catalogResourceID )
+      .concat( " dbHost: " ).concat( dbHost )
+      .concat( " dbPort: " ).concat( dbPort )
+      .concat( " dbSchema: " ).concat( dbSchema );
+  }
+
+  @Override
+  public boolean equals( Object o ) {
+    if ( o instanceof LineageDataResource ) {
+      LineageDataResource r2 = ( LineageDataResource ) o;
+      return this.shallowEquals( r2 )
+        && safeListMatch( this.getFieldLevelRelationships(), r2.getFieldLevelRelationships() );
+    } else {
+      return false;
+    }
+  }
+
+  public boolean shallowEquals( LineageDataResource r2 ) {
+    return safeStringMatch( this.getName(), r2.getName() )
+      && safeStringMatch( this.getPath(), r2.getPath() )
+      && safeStringMatch( this.getDbSchema(), r2.getDbSchema() )
+      && safeStringMatch( this.getDbHost(), r2.getDbHost() )
+      && safeStringMatch( this.getDbPort(), r2.getDbPort() )
+      && safeStringMatch( this.getDbName(), r2.getDbName() )
+      && safeListMatch( this.getFields(), r2.getFields() )
+      && safeStringMatch( this.getCatalogResourceID(), r2.getCatalogResourceID() );
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash( name, path, fields, fieldRelationships, catalogResourceID, vertexId, dbSchema, dbName, dbHost, dbPort );
+  }
+
+}
